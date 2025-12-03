ac69f993396cd7f8ab6d2b75aeddc58ac1e55462
Munkhdorj <64317250+munkhdorj-m@users.noreply.github.com>
2025-12-03 10:44:57 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,17 +1,61 @@
-# You can remove 'pass' if you written code in the function 
-
 # Exercise 1
-def average_of_digits(n):
-    pass
+# Find the average of the digits of a given number
+def average_of_digits(num):
+    total = 0
+    count = 0
+    n = num
+
+    while n > 0:
+        digit = n % 10
+        total += digit
+        count += 1
+        n //= 10
+
+    return total / count
+
 
 # Exercise 2
-def product_smallest_largest(n):
-    pass
+# Find the product of the smallest and largest digits of a given number
+def product_smallest_largest(num):
+    n = num
+    smallest = 9
+    largest = 0
+
+    while n > 0:
+        digit = n % 10
+        if digit < smallest:
+            smallest = digit
+        if digit > largest:
+            largest = digit
+        n //= 10
+
+    return smallest * largest
+
 
 # Exercise 3
-def count_digits_divisible_by_3(n):
-    pass
+# Count digits divisible by 3
+def count_digits_divisible_by_3(num):
+    n = num
+    count = 0
+
+    while n > 0:
+        digit = n % 10
+        if digit % 3 == 0:
+            count += 1
+        n //= 10
+
+    return count
+
 
 # Exercise 4
-def reverse_number(n):
-    pass
+# Find and return reverse of a number
+def reverse_number(num):
+    n = num
+    rev = 0
+
+    while n > 0:
+        digit = n % 10
+        rev = rev * 10 + digit
+        n //= 10
+
+    return rev
